8ff4da3e2dc38176815ef44e2196b0123e1c5999
emacimino <100288601+emacimino@users.noreply.github.com>
2022-09-13 13:46:01 +0200

    test 2 commit

--- test.vhd
+++ test.vhd
@@ -2,3 +2,5 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity decoder_2_to_4_w_enable is
    port(EN, A0, A1: in std_logic;)
+   D0, D1, D2, D3: out std_logic);
+   end decoder_2_to_4_w_enable;
