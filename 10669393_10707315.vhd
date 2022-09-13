3ff80d6f654655c719770c99e45a2a94af5c246a
emacimino <100288601+emacimino@users.noreply.github.com>
2022-09-13 15:51:31 +0200

    appunti ed Entity

--- 10669393_10707315.vhd
+++ 10669393_10707315.vhd
@@ -1,1 +1,16 @@
 library ieee;
+
+
+entity project_reti_logiche is
+    port (
+    i_clk : in std_logic;                           --i_clk è il segnale di CLOCK in ingresso generato dal TestBench;
+    i_rst : in std_logic;                           --i_rst è il segnale di RESET che inizializza la macchina pronta per ricevere il primo segnale di START;
+    i_start : in std_logic;                         --i_start è il segnale di START generato dal Test Bench;
+    i_data : in std_logic_vector(7 downto 0);       --i_data è il segnale (vettore) che arriva dalla memoria in seguito ad una richiesta di lettura;
+    o_address : out std_logic_vector(15 downto 0);  --o_address è il segnale (vettore) di uscita che manda l’indirizzo alla memoria;
+    o_done : out std_logic;                         --è il segnale di uscita che comunica la fine dell’elaborazione e il dato di uscita scritto in memoria;
+    o_en : out std_logic;                           --o_en è il segnale di ENABLE da dover mandare alla memoria per poter comunicare (sia in lettura che in scrittura);
+    o_we : out std_logic;                           --o_we è il segnale di WRITE ENABLE da dover mandare alla memoria (=1) per poter scriverci. Per leggere da memoria esso deve essere 0;
+    o_data : out std_logic_vector (7 downto 0)      --o_data è il segnale (vettore) di uscita dal componente verso la memoria.
+    );
+    end project_reti_logiche;
