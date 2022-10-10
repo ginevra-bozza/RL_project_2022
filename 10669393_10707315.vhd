54b27b2198816fa57841169b4f7d7ee3d6acdff6
emacimino <100288601+emacimino@users.noreply.github.com>
2022-10-10 12:42:34 +0200

    riscrittura progetto

--- 10669393_10707315.vhd
+++ 10669393_10707315.vhd
@@ -31,24 +31,113 @@ entity project_reti_logiche is
         signal current_address_write: std_logic_vector(15 downto 0); 
         signal intermediate_o_data: std_logic_vector(7 downto 0);
         signal s: bit;
+
+        signal i_data_bit: bit;
+        signal i_data_elab: std_logic_vector(1 downto 0);
+        signal i_data_counter: integer := 0 ;
+        signal R0,R1,R2,R3,R4,R5,R6,R7 : std_logic_vector(0 to 1);
         
         --reset segnali di lettura e scrittura
         signal rst_address_read: std_logic_vector(15 downto 0)  := "0000000000000000";
         signal rst_address_write: std_logic_vector(15 downto 0) := "0000001111101000"; 
 
-         function is_even(num: integer) return boolean is
-            variable even : boolean;
-        begin
-            if(num = "0" or num = "2" or num = "4" or num = "6") then
-                even := true;
-            else
-                even := false;
-            end if;
-            return even;
-            
-        end function;
         
         begin
+
+            process(i_data_bit,i_data_counter)
+            begin
+                
+                case current_state is
+                    when zero_zero => 
+                    if(i_data_bit = '0') then
+                        next_state <= zero_zero;
+                        i_data_elab <= "00";
+                    elsif(i_data_bit = '1') then
+                        next_state <= one_zero;
+                        i_data_elab <= "11";
+                    end if;
+                when one_zero => 
+                    if(i_data_bit = '0') then
+                        next_state <= zero_one;
+                        i_data_elab <= "01";
+                    elsif(i_data_bit = '1') then
+                        next_state <= one_one;
+                        i_data_elab <= "10";
+                    end if;
+                when one_one => 
+                    if(i_data_bit = '0') then
+                        next_state <= zero_one;
+                        i_data_elab <= "10";
+                    elsif(i_data_bit = '1') then
+                        next_state <= one_one;
+                        i_data_elab <= "01";
+                    end if;
+                when zero_one => 
+                    if(i_data_bit = '0') then
+                        next_state <= zero_zero;
+                        i_data_elab <= "11";
+                    elsif(i_data_bit = '1') then
+                        next_state <= one_zero;
+                        i_data_elab <= "00";
+                    end if;     
+                end case;
+                case i_data_counter is
+                    when 0 =>
+                        R0 <= i_data_elab;
+                    when 1 =>
+                        R1 <= i_data_elab;
+                    when 2 =>
+                        R2 <= i_data_elab;
+                    when 3 =>
+                        R3 <= i_data_elab;
+                    when 4 =>
+                        R4 <= i_data_elab;
+                    when 5 =>
+                        R5 <= i_data_elab;
+                    when 6 =>
+                        R6 <= i_data_elab;
+                    when 7 =>
+                        R7 <= i_data_elab;
+                    end case;
+            end process;
+
+    process(i_clk,i_rst,i_start,i_data)
+    begin
+        if(rising_edge(i_clk)) then
+            if(i_rst = '1') then
+                current_address_read <= rst_address_read;
+                    current_address_write <= rst_address_write;
+                    o_en <= '0';
+                    o_we <= '0';
+                    o_data <= "00000000";
+                    o_done <= '0';
+                    current_state <= zero_zero;
+            elsif(i_start = '1') then
+                i_data_bit <= i_data(i_data_counter);
+                i_data_counter <= i_data_counter + 1;
+                current_state <= next_state;
+                --gestire if nel caso o_end e fare il concatenamento
+            end if;
+        end if;
+    end process;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             process( i_clk, i_rst ,i_start, o_done, o_address, current_address_read,current_address_write,o_en,o_we,current_state,program_state)
             begin
                 if(i_rst = '1') then
